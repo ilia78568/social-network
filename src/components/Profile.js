210ad4a4346f7f26efa4d9c8fbdeeef29eaea565
ilia8585 <ilia19.99@mail.ru>
2020-12-11 10:23:54 +0300

    init: start project and commiting

---
+++ src/components/Profile.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <div className="content">
+    <div>
+      <img src="https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg"/>
+    </div>
+    <div>
+      ava+descr
+    </div>
+    <div>
+      My post
+      <div>
+        Pos1
+      </div>
+      <div>
+        Pos2
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default Profile;
