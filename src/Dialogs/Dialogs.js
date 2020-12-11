3d400d7336ea20adbd61f30f97226bef1e6ab2b5
ilia8585 <ilia19.99@mail.ru>
2020-12-11 12:38:11 +0300

    add user and messages for dialogs

--- src/Dialogs/Dialogs.js
+++ src/Dialogs/Dialogs.js
@@ -1,11 +1,30 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import styles from './Dialogs.module.css'
 
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={styles.dialogs}>
+           <div className={styles.dialogsItems}>
+                <NavLink to='/dialogs/1' className={styles.dialog}>
+                    Ilia
+                </NavLink>
+                <NavLink to='/dialogs/2' className={styles.dialog}>
+                    Петя
+                </NavLink>
+                <NavLink to='/dialogs/3' className={styles.dialog}>
+                    Грыща
+                </NavLink>
+                <NavLink to='/dialogs/4' className={styles.dialog}>
+                    Салам
+                </NavLink>
+           </div>
+            <div className={styles.messages}>
+                <div className="message">Hi people</div>
+                <div className="message">do u like reading</div>
+                <div className="message">i love us</div>
+            </div>
         </div>
     )
 }
