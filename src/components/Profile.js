4cc750e00e440a2d25e7f1dbe75c071d19746011
ilia8585 <ilia19.99@mail.ru>
2020-12-11 10:43:46 +0300

    feat: work with module.css

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,20 +1,22 @@
 import React from 'react'
+import styles from './Profile.module.css'
+
 
 const Profile = () => {
   return (
-    <div className="content">
+    <div className={styles.content}>
     <div>
       <img src="https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg"/>
     </div>
     <div>
       ava+descr
     </div>
-    <div>
+    <div className="posts">
       My post
-      <div>
+      <div className={styles.item}>
         Pos1
       </div>
-      <div>
+      <div className={styles.item}>
         Pos2
       </div>
     </div>
