38fc1e01e315873876d84692a73af873fb6b7167
ilia8585 <ilia19.99@mail.ru>
2020-12-11 18:52:33 +0300

    create ref textarea

--- src/components/Profile/MyPosts/MyPosts.js
+++ src/components/Profile/MyPosts/MyPosts.js
@@ -6,12 +6,19 @@ import Post from './Post/Post'
 const MyPosts = ({postData}) => {
  
   let posts = postData.map(elem => <Post message={elem.message} likeCount={elem.likeCount}/>)
+  
+  let newPostElement = React.createRef();
+  let addPost = () => {
+    let text = newPostElement.current.value;
+    alert(text);
+  }
+
   return (
     <div className="posts">
       My post
       <div>
-        <textarea></textarea>
-        <button>Add post</button>
+        <textarea ref={newPostElement}></textarea>
+        <button onClick={addPost}>Add post</button>
       </div>
       {posts}
     </div>
