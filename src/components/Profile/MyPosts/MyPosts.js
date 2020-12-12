8a3a33da727d09d6ac4f80423ec75dc79fd08f87
ilia8585 <ilia19.99@mail.ru>
2020-12-12 22:01:14 +0300

    add action creators

--- src/components/Profile/MyPosts/MyPosts.js
+++ src/components/Profile/MyPosts/MyPosts.js
@@ -1,7 +1,7 @@
 import React from 'react'
 // import styles from './MyPosts.module.css'
 import Post from './Post/Post'
-
+import {addPostActionCreator, onPostChangeActionCreator} from '../../../redux/state'
 
 const MyPosts = (props) => {
  
@@ -9,14 +9,14 @@ const MyPosts = (props) => {
   
   let newPostElement = React.createRef();
   
+  
   let addPost = () => {
-    props.dispatch( {type: 'ADD-POST'});
-
+    props.dispatch( addPostActionCreator());
   }
 
   let onPostChange = () => {
     let text = newPostElement.current.value;
-    props.dispatch( {type: 'UPDATE-NEW-POST-TEXT', newText : text})
+    props.dispatch(onPostChangeActionCreator(text))
   }
 
   return (
