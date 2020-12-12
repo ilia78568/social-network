97349e02bd952757d2fe83fe4284b2395c44e566
ilia8585 <ilia19.99@mail.ru>
2020-12-12 15:30:36 +0300

    refactor: add subscribe, delete render

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,6 @@
-import {rerenderEntireTree} from '../render'
+let rerenderEntireTree = () => {
+    console.log('state changed')
+}
 
 let state = {
     profilePage: {
@@ -38,4 +40,9 @@ export let updateNewPostText = (newText => {
     state.profilePage.newPostText = newText
     rerenderEntireTree(state)
 })
+
+export const subscribe = (observer) => {
+    rerenderEntireTree = observer
+}
+
 export default state;
