c300a64ccbc8e0535f1262899a07bc1fbf8cf104
ilia8585 <ilia19.99@mail.ru>
2020-12-14 10:39:03 +0300

    add axios get for User

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import styles from './users.module.css'
+import * as axios from 'axios'
+import userPhoto from '../assets/images/user.jpg'
 
 const Users = (props) => {
     
     if (props.users.length === 0) {
-        props.setUsers([
-            {id: 1,  followed: false, fullName: 'ILIA', status: 'ima boss', location: {city: 'Minsk', country: 'Belarus'}, photoUrl: 'https://vokrug-tv.ru/pic/person/6/e/0/2/6e02e4d5202366442710552912e7a10f.jpg' },
-            {id: 2,  followed: true, fullName: 'ILIA', status: 'ima boss', location: {city: 'Minsk', country: 'Belarus'}, photoUrl: 'https://vokrug-tv.ru/pic/person/6/e/0/2/6e02e4d5202366442710552912e7a10f.jpg' },
-            {id: 3,  followed: true, fullName: 'ILIA', status: 'ima boss', location: {city: 'Minsk', country: 'Belarus'}, photoUrl: 'https://vokrug-tv.ru/pic/person/6/e/0/2/6e02e4d5202366442710552912e7a10f.jpg' },
-            {id: 4,  followed: false, fullName: 'ILIA', status: 'ima boss', location: {city: 'Minsk', country: 'Belarus'}, photoUrl: 'https://vokrug-tv.ru/pic/person/6/e/0/2/6e02e4d5202366442710552912e7a10f.jpg' },
-            ])
+        
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(res => {
+                props.setUsers(res.data.items)
+            })
+        
+        // props.setUsers([
+        //     {id: 1,  followed: false, fullName: 'ILIA', status: 'ima boss', location: {city: 'Minsk', country: 'Belarus'}, photoUrl: 'https://vokrug-tv.ru/pic/person/6/e/0/2/6e02e4d5202366442710552912e7a10f.jpg' },
+        //     {id: 2,  followed: true, fullName: 'ILIA', status: 'ima boss', location: {city: 'Minsk', country: 'Belarus'}, photoUrl: 'https://vokrug-tv.ru/pic/person/6/e/0/2/6e02e4d5202366442710552912e7a10f.jpg' },
+        //     {id: 3,  followed: true, fullName: 'ILIA', status: 'ima boss', location: {city: 'Minsk', country: 'Belarus'}, photoUrl: 'https://vokrug-tv.ru/pic/person/6/e/0/2/6e02e4d5202366442710552912e7a10f.jpg' },
+        //     {id: 4,  followed: false, fullName: 'ILIA', status: 'ima boss', location: {city: 'Minsk', country: 'Belarus'}, photoUrl: 'https://vokrug-tv.ru/pic/person/6/e/0/2/6e02e4d5202366442710552912e7a10f.jpg' },
+        //     ])
     }
     
 
@@ -19,7 +27,7 @@ const Users = (props) => {
                    return <div key={elem.id}>
                        <span>
                            <div>
-                               <img src={elem.photoUrl} className={styles.userPhoto}/>
+                               <img src={elem.photos.small != null ? elem.photos.small : userPhoto} className={styles.userPhoto}/>
                            </div>
                            <div>
                                {elem.followed 
@@ -30,12 +38,12 @@ const Users = (props) => {
                        </span>
                        <span>
                            <span>
-                                <div>{elem.fullName}</div>
+                                <div>{elem.name}</div>
                                 <div>{elem.status}</div>
                            </span>
                            <span>
-                                <div>{elem.location.city}</div>
-                                <div>{elem.location.country}</div>
+                                <div>{"elem.location.city"}</div>
+                                <div>{"elem.location.country"}</div>
                            </span>
                        </span>
                    </div>
