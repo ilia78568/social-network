a7b37ea207ad9fb78788cd033bf28d07884b4163
ilia8585 <ilia19.99@mail.ru>
2020-12-17 13:31:23 +0300

    create ProfileStatus component with localstate

--- src/components/Profile/ProfileInfo/ProfileInfo.js
+++ src/components/Profile/ProfileInfo/ProfileInfo.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Preloader from '../../common/Preloader/Preloader'
 import s from './ProfileInfo.module.css'
+import ProfileStatus from './ProfileStatus'
 
 const ProfileInfo = (props) => {
   if(!props.profile) {
@@ -9,12 +10,16 @@ const ProfileInfo = (props) => {
   
   return (
     <div >
-    <div>
+    {/* <div>
       <img alt="alt" src="https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg"/>   
-    </div>
+    </div> */}
     <div className={s.descriptionBlock}>
    
       <img src={props.profile.photos.large}/>
+
+      <ProfileStatus status={'gfgfgf'}/>
+
+
       <div>{props.profile.fullName}</div>
       <div>{props.profile.lookingForAJobDescription}</div>
     </div>
